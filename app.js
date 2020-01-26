1ecf9fa57840fc3695aaa539e52457f1a979ac02
MattRickS <mattalexshaw@gmail.com>
2020-01-26 20:52:36 +0000

    add comments and styled campground show page

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express"),
 	  bodyParser = require("body-parser"),
 	  mongoose = require("mongoose"),
 	  app = express(),
-      Campground = require("./models/campground");
+      Campground = require("./models/campground"),
+      Comment = require("./models/comment"),
+      seedDB = require("./seeds");
 
 // TODO: Watch https://www.youtube.com/watch?v=NHHh0sj1uKY to update
 // from bootstrap 3.3 to 4
@@ -15,8 +17,10 @@ mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
+// seedDB();
 
 app.get("/", function(req, res) {
 	res.render("landing");
@@ -52,11 +56,48 @@ app.get("/campgrounds/new", function(req, res) {
 });
 // SHOW
 app.get("/campgrounds/:id", function(req, res) {
-	Campground.findById(req.params.id, function(err, campground) {
+	Campground.findById(req.params.id).populate("comments").exec(function(err, campground) {
 		if (err || !campground) {
 			res.send("Failed to retrieve campground for this id!");
 		} else {
+			console.log(campground);
 			res.render("campgrounds/show", {campground: campground});
+		}
+	});
+});
+
+// =============================================================================
+// COMMENTS
+// =============================================================================
+
+// NEW
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	})
+});
+app.post("/campgrounds/:id/comments", function(req, res) {
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if (err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save(function(err, camp) {
+						if (err) {
+							console.log(err);
+						}
+					});
+					res.redirect(`/campgrounds/${campground._id}`);
+				}
+			});
 		}
 	});
 });
--- models/campground.js
+++ models/campground.js
@@ -5,5 +5,11 @@ var campgroundSchema = new mongoose.Schema({
 	name: String,
 	image: String,
 	description: String,
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Comment",
+		}
+	],
 });
 module.exports = mongoose.model("Campground", campgroundSchema);
