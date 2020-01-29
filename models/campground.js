36baf2582abca02e1eaaa8af3bf265b277e3b970
MattRickS <mattalexshaw@gmail.com>
2020-01-29 22:30:07 +0000

    Add author to campgrounds

--- models/campground.js
+++ models/campground.js
@@ -11,5 +11,12 @@ var campgroundSchema = new mongoose.Schema({
 			ref: "Comment",
 		}
 	],
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+		username: String,
+	},
 });
 module.exports = mongoose.model("Campground", campgroundSchema);
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,11 +22,17 @@ router.get("/", function(req, res) {
 	});
 });
 // CREATE
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var description = req.body.description;
-	var campground = {name: name, image: image, description: description};
+	var author = {
+		id: req.user._id,
+		username: req.user.username,
+	}
+	var campground = {
+		name: name, image: image, description: description, author: author
+	};
 	Campground.create(campground, function(err, campground) {
 		if (err) {
 			console.log(err);
